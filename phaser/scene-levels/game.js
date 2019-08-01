682fad31be42d906ff1a4e4640436edeb885aa1b
Stanley Seow <stanleyseow@gmail.com>
2019-08-01 17:37:46 +0800

    Added Auto Resize to scene-level

--- phaser/scene-levels/game.js
+++ phaser/scene-levels/game.js
@@ -1,6 +1,7 @@
 
 let config = {
     type: Phaser.AUTO,
+    parent	: 'phaser-app',
     width: 800,
     height: 600,
     backgroundColor: '#000055',
@@ -21,6 +22,35 @@ let config = {
 };
 
 let game = new Phaser.Game(config);
+
+function resizeApp ()
+{
+    // Width-height-ratio of game resolution
+    // Replace 360 with your game width, and replace 640 with your game height
+    let game_ratio = 360 / 640;
+	
+    // Make div full height of browser and keep the ratio of game resolution
+    let div = document.getElementById('phaser-app');
+    div.style.width = (window.innerHeight * game_ratio) + 'px';
+    div.style.height = window.innerHeight + 'px';
+	
+    // Check if device DPI messes up the width-height-ratio
+    let canvas	= document.getElementsByTagName('canvas')[0];
+	
+    let dpi_w	= parseInt(div.style.width) / canvas.width;
+    let dpi_h	= parseInt(div.style.height) / canvas.height;		
+	
+    let height	= window.innerHeight * (dpi_w / dpi_h);
+    let width	= height * game_ratio;
+	
+    // Scale canvas	
+    canvas.style.width	= width + 'px';
+    canvas.style.height	= height + 'px';
+}
+
+window.addEventListener('resize', resizeApp);
+
+
 
 
 
