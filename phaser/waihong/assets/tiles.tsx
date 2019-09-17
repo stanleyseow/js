215a5c90e86448b60c30a82e8b67ee7ee486b49c
Stanley Seow <stanleyseow@gmail.com>
2019-09-17 18:17:11 +0800

    Deleted redundant stuff

--- phaser/waihong/assets/tiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.4" name="tiles" tilewidth="64" tileheight="64" tilecount="9" columns="3">
- <image source="tiles.png" width="247" height="251"/>
-</tileset>
